9b176de2a37ad56d8f5d867a442e85a460cc8558
Millym94 <Millym94@gmail.com>
2023-08-19 21:39:38 -0400

    Update script.js
    .gitignore

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let pokemonList = [
     { name: 'Pikachu', height: 0.4, types: 'electric' },
     { name: 'Charizard', height: 1.7, types: ['fire', 'flying'] },
-    { name: 'Mewtwo', height: 2, types: 'psychic'}
+    { name: 'Mewtwo', height: 2, types: 'psychic' }
 ]
