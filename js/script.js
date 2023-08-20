ce0e0c314cb4908de73ac9849b032539e4fb5f4e
Millym94 <Millym94@gmail.com>
2023-08-19 21:37:17 -0400

    Update script.js
    if applied this commit will capitalize the L in pokemonList

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let pokemonlist = [
+let pokemonList = [
     { name: 'Pikachu', height: 0.4, types: 'electric' },
     { name: 'Charizard', height: 1.7, types: ['fire', 'flying'] },
     { name: 'Mewtwo', height: 2, types: 'psychic'}
