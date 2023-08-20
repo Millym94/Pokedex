c53bbb40f977c8aeae127eb7d91e3b038dd3d738
Millym94 <Millym94@gmail.com>
2023-08-19 21:33:23 -0400

    Update script.js
    If applied this commit will create a new variable 'pokemon list and add objects to the array. it will list 3 objects (pokemon) and their values

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let pokemonlist = [
+    { name: 'Pikachu', height: 0.4, types: 'electric' },
+    { name: 'Charizard', height: 1.7, types: ['fire', 'flying'] },
+    { name: 'Mewtwo', height: 2, types: 'psychic'}
+]
