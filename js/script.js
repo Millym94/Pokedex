5b234c966a0f80f8dd0152bad531a0c46f795d0d
Millym94 <Millym94@gmail.com>
2023-09-04 18:47:27 -0400

    Update script.js
    if added this commit will: use document.write() inside the loop's code to write the Pokemon on my website's DOM

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@ let pokemonList = [
     { name: 'Charizard', height: 1.7, types: ['fire', 'flying'] },
     { name: 'Mewtwo', height: 2, types: 'psychic' }
 ]
+
+for (let i = 0; 1 < pokemonList.length; i++) {
+    document.write('${pokemonList[I].name} (height: ${pokemonList[i].height}) ')
+
+    if (pokemonList[i].height > 1.8) {
+        document.write('${pokemonList[i].name} (height:${pokemonList[i].height}) - Wow, That is a big Pokemon!')
+
+    } else {
+        document.write(' ${pokemonList[i].name} (height: ${pokemonList[i].height}) ')
+    }
+}
