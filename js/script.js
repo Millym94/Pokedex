c320ebce96e755ba7e2dc9bd5c1e766e2e011647
Millym94 <Millym94@gmail.com>
2023-10-06 16:44:16 -0400

    Update script.js
    - Manipulated the DOM
    - Added a button for each Pokemon Array
    - Added an event listener

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let pokemonRepository = (function () {
-    // array to store pokemon data
-    let pokemonList = [
+    // array to store pokemon data //
+    let repository = [
     { 
         name: 'Pikachu',
         height: 0.4,
@@ -19,33 +19,61 @@ let pokemonRepository = (function () {
         types: 'psychic'
     }
 ];
- // Function to get all Pokemon in the repository 
-function getAll() {
-    return pokemonList;
+
+// Function to add a new Pokemon to the repository //
+    function add(pokemon) {
+        if (
+            typeof pokemon === `object` &&
+            'name' in pokemon &&
+            `height` in pokemon &&
+            `types` in pokemon
+        ) {
+            repository.push(pokemon);
+        } else {
+            console.log(`pokemon is not correct`);
+        }
 }
 
-// Function to add a new Pokemon to the Repository
-function add(pokemon) {
-    pokemonList.push(pokemon);
-}
-
-// Return an object that can be accessed from the outside of the function
-return {
-    getAll: getAll,
-    add: add,
-     };
-})();
-
-// Log result of getAll() to console
-console.log(pokemonRepository.getAll());
-
-// Iterate over Pokemon using forEach
-pokemonRepository.getAll().forEach(function (pokemon) {
-    let pokemonDetails = pokemon.name + ` (height: `+ pokemon.height +`)`;
-
-    if (pokemon.height > 1.8) {
-        pokemonDetails += `- Wow, that is a big Pokemon!`;
+// Function to get all Pokemon in the repository //
+    function getAll() {
+        return repository;
     }
 
-    document.write(pokemonDetails + `<br>`); // line break for formatting
+// Function to add a list item with a button for a Pokemon //
+    function addListItem(pokemon) {
+        let pokemonList = document.querySelector(`.pokemon-list`);
+        let listpokemon = document.createElement(`li`);
+        let button = document.createElement(`button`);
+        button.innerText = pokemon.name;  // Set the button's text to the Pokemon's name //
+        button.classList.add(`button-class`);
+        button.addEventListener(`click`, function() {   //add a click event listener to the button //
+            console.log(pokemon.name + ` was chosen!`);
+        });
+
+// Append the button to the list and the list item to the Pokemon List //
+        listpokemon.appendChild(button);
+        pokemonList.appendChild(listpokemon);
+    }
+
+// Function to show Pokemon details (currently just logs to the console) //
+    function showDetails(pokemon) {
+        console.log(pokemon);
+    }
+  
+    return {
+        add:add,
+        getAll: getAll,
+        addListItem: addListItem
+    };
+})();
+
+// Add a new Pokemon to the repository //
+pokemonRepository.add({ name: `Gyarados`, height: 6.5, types: [`water`, `flying`] });
+
+// Log all Pokemon in the repository //
+console.log(pokemonRepository.getAll());
+
+// Iterate over the Pokemon list & add items with buttons for each //
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
 });
